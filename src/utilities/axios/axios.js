dc5bf7d1f0d6d6cba35c17c301986a6222be1826
alexgordontlv <alexaybn@gmail.com>
2021-03-12 12:01:59 +0200

    pushing to heroku

--- src/utilities/axios/axios.js
+++ src/utilities/axios/axios.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
+let BASE_URL = '';
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+	BASE_URL = 'http://localhost:5000';
+} else {
+	BASE_URL = window.location?.href;
+}
 
 export default axios.create({
-	baseURL: 'http://localhost:5000',
+	baseURL: BASE_URL,
 });
