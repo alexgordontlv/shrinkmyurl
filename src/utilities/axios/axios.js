f42e3d968017db0413b02abefa737cc3e836798b
alexgordontlv <alexaybn@gmail.com>
2021-03-12 12:37:00 +0200

    pushing to heroku

--- src/utilities/axios/axios.js
+++ src/utilities/axios/axios.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 let BASE_URL = '';
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-	BASE_URL = 'http://localhost:5000';
-} else {
-	BASE_URL = 'https://simple-portal.herokuapp.com/';
-}
 
 export default axios.create({
-	baseURL: BASE_URL,
+	baseURL: process.env.baseURL || 'http://localhost:5000',
 });
