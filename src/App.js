3a002e6e0820ec8e71d8096e26576f9f20d8f5f4
alexgordontlv <alexaybn@gmail.com>
2021-04-19 23:10:27 +0300

    open modal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Header from './components/header/Header2';
 import Mainpage from './pages/main/Mainpage';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { useUserContext } from './context/user.context';
+import Modal from './components/modal/Modal';
+import { handleModal, modalState } from './utilities/uitiltyfunctions/handleModal';
 import './app.styles.css';
 function App() {
 	const context = useUserContext();
 	useEffect(() => {
+		handleModal('open');
 		const loggedInUser = localStorage.getItem('currentUser');
 		if (loggedInUser) {
 			const foundUser = JSON.parse(loggedInUser);
@@ -17,6 +20,7 @@ function App() {
 	return (
 		<Router>
 			<div className='app '>
+				<Modal setOpenModal={handleModal} />
 				<Header />
 				<Mainpage />
 			</div>
