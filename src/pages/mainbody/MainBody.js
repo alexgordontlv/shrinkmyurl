f51fe3c0bf5e64baa9647c79f260a5f81af5b7a2
alexgordontlv <alexaybn@gmail.com>
2021-04-24 16:23:20 +0300

    grid

--- src/pages/mainbody/MainBody.js
+++ src/pages/mainbody/MainBody.js
@@ -4,15 +4,18 @@ import LinkForm from '../../components/linkform/LinkForm';
 const MainBody = () => {
 	return (
 		<div className='grid grid-rows-3 grid-flow-col gap-4'>
-			<div className='row-span-3'>
+			<div className='row-span-3 bg-red-100'>
+				<LinkForm />
+				<LinkForm />
 				<LinkForm />
 			</div>
-			<div className='row-span-2'>
+			<div className='col-span-2 bg-red-200'>
 				<LinkForm />
 			</div>
-			<div className='row-span-2 col-span-2'>
+			<div className='row-span-2 col-span-2 bg-red-300'>
 				<LinkForm />
 				<LinkForm />
+
 				<LinkForm />
 			</div>
 		</div>
