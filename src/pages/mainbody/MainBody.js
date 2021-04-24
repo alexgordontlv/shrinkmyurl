2e822d01a096b65e8f124dbebdc893648cbb4723
alexgordontlv <alexaybn@gmail.com>
2021-04-24 19:12:38 +0300

    grid2

--- src/pages/mainbody/MainBody.js
+++ src/pages/mainbody/MainBody.js
@@ -3,20 +3,21 @@ import { LockClosedIcon } from '@heroicons/react/solid';
 import LinkForm from '../../components/linkform/LinkForm';
 const MainBody = () => {
 	return (
-		<div className='grid grid-rows-3 grid-flow-col gap-4'>
-			<div className='row-span-3 bg-red-100'>
-				<LinkForm />
-				<LinkForm />
-				<LinkForm />
-			</div>
-			<div className='col-span-2 bg-red-200'>
-				<LinkForm />
-			</div>
-			<div className='row-span-2 col-span-2 bg-red-300'>
-				<LinkForm />
-				<LinkForm />
+		<div className='flex  justify-center'>
+			<div className='grid grid-cols-1 max-w-5xl md:grid-cols-3 md:grid-rows-4 gap-2 '>
+				<div className='col-span-2 bg-red-100'>
+					<LinkForm />
+				</div>
+				<div className=' row-span-3  bg-red-200 '>
+					<LinkForm />
+				</div>
 
-				<LinkForm />
+				<div className='row-span-2 col-span-2 bg-red-300'>
+					<LinkForm />
+				</div>
+				<div className='row-span-3 col-span-4 bg-red-600'>
+					<LinkForm />
+				</div>
 			</div>
 		</div>
 	);
