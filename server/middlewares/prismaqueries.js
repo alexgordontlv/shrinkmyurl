7c12b2f390cd66813a3bae0ab3c4874179f5bdef
alexgordontlv <alexaybn@gmail.com>
2021-05-13 11:47:45 +0300

    add http check

--- server/middlewares/prismaqueries.js
+++ server/middlewares/prismaqueries.js
@@ -42,7 +42,10 @@ const loginUser = async (req, res, next) => {
 };
 
 const createUrl = async (req, res, next) => {
-	const { originalUrl, email } = req.body;
+	let { originalUrl, email } = req.body;
+	if (!originalUrl.includes('http')) {
+		originalUrl = 'https://' + originalUrl;
+	}
 	const randomId = Math.floor((1 + Math.random()) * 0x1000000)
 		.toString(16)
 		.substring(1);
