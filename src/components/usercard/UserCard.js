fbaaf2462f0595ecc29c64d0e1dd41643e7b0f45
alexgordontlv <alexaybn@gmail.com>
2021-05-15 12:30:26 +0300

    profile card

--- src/components/usercard/UserCard.js
+++ src/components/usercard/UserCard.js
@@ -1,53 +1,49 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, Avatar, CardActionArea, CardActions, Typography } from '@material-ui/core/';
-import FormDialog from '../formdialog/FormDialog';
-import DeleteDialog from '../deletedialog/DeleteDialog';
+import Spinner from '../spinner/Spinner';
+import { useQuery } from 'react-query';
+import axios from '../../utilities/axios/axios';
+import { useUserContext } from '../../context/user.context';
+import WrapperCard from '../wrappercard/WrapperCard';
+export default function UserCard() {
+	const {
+		state: { currentUser },
+	} = useUserContext();
 
-const useStyles = makeStyles({
-	rootDiv: {
-		display: 'flex',
-		justifyContent: 'center',
-	},
-	root: {
-		margin: '5px',
-		width: 200,
-	},
-	avatarDiv: {
-		height: 50,
-		marginLeft: '15px',
-		display: 'flex',
-		justifyContent: 'flex-start',
-		alignItems: 'center',
-	},
-});
+	const { isLoading, error, data } = useQuery('links', async () => {
+		const { data } = await axios.get(`/users/${currentUser.id}`);
+		return data;
+	});
 
-export default function UserCard(props) {
-	let { name, id, email, role } = props.location.state;
-	const { setRender } = props;
-	const classes = useStyles();
+	if (isLoading) return <Spinner />;
+	if (error) return 'An error has occurred: ' + error.message;
 
 	return (
-		<div className={classes.rootDiv}>
-			<Card className={classes.root}>
-				<CardActionArea>
-					<div className={classes.avatarDiv}>
-						<Avatar />
+		<div className='flex justify-center mt-10'>
+			<div className='rounded rounded-t-lg overflow-hidden shadow max-w-xl my-3'>
+				<img alt='logo' src='https://i.imgur.com/dYcYQ7E.png' className='w-full' loading='lazy' />
+				<div className='flex justify-center -mt-8'>
+					<img
+						alt='profile'
+						src='https://i.imgur.com/8Km9tLL.jpg'
+						className='rounded-full border-solid border-white border-2 -mt-3'
+						loading='lazy'
+					/>
+				</div>
+				<div className='text-center px-3 pb-6 pt-2'>
+					<h3 className='text-black text-lg font-normal '>{`Email: ${data.email}`}</h3>
+					<p className='mt-2 text-grey-dark text-md font-normal'>{`Role: ${data.role}`}</p>
+				</div>
+				<div className='flex justify-center pb-3 text-grey-dark'>
+					<div className='text-center mr-3 border-r pr-3'>
+						<h2>34</h2>
+						<span>Links</span>
 					</div>
-					<CardContent>
-						<Typography gutterBottom variant='h5' component='h2'>
-							{name}
-						</Typography>
-						<Typography variant='body1' color='textSecondary'>
-							{email}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<FormDialog name={name} id={id} email={email} role={role} setRender={setRender} />
-					<DeleteDialog id={id} setRender={setRender} />
-				</CardActions>
-			</Card>
+					<div className='text-center'>
+						<h2>42</h2>
+						<span>Total Views</span>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
