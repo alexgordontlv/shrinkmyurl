cc206205140f050b91713005d0ec16ffdc24a672
alexgordontlv <alexaybn@gmail.com>
2021-05-28 16:45:28 +0300

    updated link to hashed + sort by view counts

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from '../../utilities/axios/axios';
 import { useGetCurrentUser } from '../../context/user.context';
 import { useQuery } from 'react-query';
@@ -6,11 +6,18 @@ import moment from 'moment';
 import Spinner from '../../components/spinner/Spinner';
 
 const Dashboard = () => {
+	const [flag, setFlag] = useState(false);
 	const currentUser = useGetCurrentUser();
 	const { isLoading, error, data } = useQuery('users', async () => {
 		const { data } = await axios.get(`/userurls/${currentUser.id}`);
 		return data;
 	});
+
+	const handleSort = () => {
+		data.sort((a, b) => (a.viewCount > b.viewCount ? 1 : -1));
+		console.log(data);
+	};
+
 	if (isLoading) return <Spinner />;
 	if (error) return 'An error has occurred: ' + error.message;
 
@@ -34,37 +41,42 @@ const Dashboard = () => {
 									<th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
 										Last Viewed
 									</th>
-									<th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+									<th
+										onClick={handleSort}
+										scope='col'
+										className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
 										View Count
 									</th>
 								</tr>
 							</thead>
 							<tbody className='bg-white divide-y divide-gray-200'>
-								{data?.map((link, idx) => (
-									<tr key={idx}>
-										<td className='px-6 py-4 whitespace-nowrap'>
-											<div className='flex items-center justify-center'>
-												<div className='ml-4'>
-													<div
-														className='text-sm font-medium text-gray-900 cursor-pointer'
-														onClick={() => window.open(link.originalUrl, '_blank')}>
-														{link.originalUrl?.length > 30 ? link.originalUrl.slice(0, 30) + '...' : link.originalUrl}
+								{data
+									.sort((a, b) => (a.viewCount > b.viewCount ? 1 : -1))
+									.map((link, idx) => (
+										<tr key={idx}>
+											<td className='px-6 py-4 whitespace-nowrap'>
+												<div className='flex items-center justify-center'>
+													<div className='ml-4'>
+														<div
+															className='text-sm font-medium text-gray-900 cursor-pointer'
+															onClick={() => window.open(`https://www.shrinkmy.site/${link.hash}`, '_blank')}>
+															{link.originalUrl?.length > 30 ? link.originalUrl.slice(0, 30) + '...' : link.originalUrl}
+														</div>
 													</div>
 												</div>
-											</div>
-										</td>
-										<td className='px-6 py-4 whitespace-nowrap'>
-											<div className='text-sm text-gray-900'>{link.hash}</div>
-										</td>
-										<td className='px-6 py-4 whitespace-nowrap'>
-											<div className='text-sm text-gray-900'>{moment(link.createdAt).format('Do MMMM YYYY')}</div>
-										</td>
-										<td className='px-6 py-4 whitespace-nowrap'>
-											<div className='text-sm text-gray-900'>{moment(link.updatedAt).format('lll ')}</div>
-										</td>
-										<td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>{link.viewCount}</td>
-									</tr>
-								))}
+											</td>
+											<td className='px-6 py-4 whitespace-nowrap'>
+												<div className='text-sm text-gray-900'>{link.hash}</div>
+											</td>
+											<td className='px-6 py-4 whitespace-nowrap'>
+												<div className='text-sm text-gray-900'>{moment(link.createdAt).format('Do MMMM YYYY')}</div>
+											</td>
+											<td className='px-6 py-4 whitespace-nowrap'>
+												<div className='text-sm text-gray-900'>{moment(link.updatedAt).format('lll ')}</div>
+											</td>
+											<td className='px-6 py-4 whitespace-nowrap text-sm text-gray-500'>{link.viewCount}</td>
+										</tr>
+									))}
 							</tbody>
 						</table>
 					</div>
