225f89418ec1f7f4631b9bed8e4847c5dcc82f64
alexgordontlv <alexaybn@gmail.com>
2021-10-24 22:53:09 +0300

    Added logger and login middleware

--- server/middlewares/adminauth.js
+++ server/middlewares/adminauth.js
@@ -1,15 +1,23 @@
 const { PrismaClient } = require('@prisma/client');
+const logger = require('../utilities/logger');
 const prisma = new PrismaClient();
-
 const adminAuth = async (req, res, next) => {
-	if (!req.user) return res.status(400).json({ err: 'There is no user' });
+	logger.indo(`starting adminAuth middleware`);
+	if (!req.user) {
+		logger.error('There is no user');
+		return res.status(400).json({ err: 'There is no user' });
+	}
 	const userById = await prisma.users.findUnique({
 		where: {
 			id: parseInt(req.user?.id),
 		},
 	});
 
-	if (!userById) return res.status(400).json({ err: 'There is no user in database' });
+	if (!userById) {
+		logger.error(`No user found with id: ${req.user.id}`);
+		return res.status(400).json({ err: 'There is no user in database' });
+	}
+	//TO DO : SPLIT ADMIN OAUTH
 	if (userById?.role === 'admin') {
 		req.users = await prisma.users.findMany();
 		return next();
